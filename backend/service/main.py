4c3f9526e00f1bfb5fb0e60096ef991a78feffaa
Roman Kotelnikov <roman.kotelnikov@vertoanalytics.com>
2017-11-25 23:42:10 +0200

    Add endpoint to fetch area aggregate values

--- backend/service/main.py
+++ backend/service/main.py
@@ -1,14 +1,17 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from urllib import quote_plus
+import json
 from requests import get
 
 from util import crossdomain
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 TOKEN_PATH = os.path.join(DIR, 'token')
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder=os.path.join(DIR, 'templates'))
+
 
 def bbox_from_point(lat, lon):
     # min_degree = 0.002253
@@ -44,11 +47,17 @@ class RomaniRequest(object):
         # just because bbox is special
         if self._params.get('bbox'):
             params_encoded['bbox'] = self._params['bbox']
+        if self._params.get('linestring'):
+            params_encoded['linestring'] = self._params['linestring']
         endpoint = ep or self.ENDPOINT
         print "{}?{}".format(endpoint, "&".join([
             "{}={}".format(k, v) for (k, v) in params_encoded.iteritems()]))
         return get(endpoint, params=params_encoded).text
 
+@app.route("/")
+@crossdomain(origin='*')
+def index():
+    return render_template("index.html")
 
 @app.route("/menu")
 @crossdomain(origin='*')
@@ -101,6 +110,46 @@ def feature_info(seriesName, feature):
         x='1',
         y='1'
     ).get()
+
+
+@app.route('/feature_info_area/<string:seriesName>/<string:feature>', methods=['POST'])
+@crossdomain(origin='*')
+def feature_info_reg(seriesName, feature):
+    lat, lon = request.get_json()
+    print lat, lon
+
+    minLat, minLon, maxLat, maxLon = bbox_from_point(lat, lon)
+
+    linestring = ",".join([
+        "{}%20{}".format(la, lo) for (la, lo) in [
+            (minLat, minLon),
+            (maxLat, minLon),
+            (maxLat, maxLon),
+            (maxLat, minLon),
+            (minLat, minLon)  # to close the square
+        ]
+    ])
+
+    print linestring
+
+    response = RomaniRequest(**{
+        'request': 'GetArea',
+        'service': 'WMS',
+        'return': 'aggregate',
+        'version': '1.1.1',
+        'crs': 'EPSG:4326',
+        'layer': seriesName + '/' + feature,
+        'linestring': linestring,
+        'format': 'text/json'
+    }).get()
+
+    print response
+
+    return json.dumps({
+        'value': json.loads(response)['area']['aggregate']['mean'],
+        'lat': lat,
+        'lng': lon
+    })
 
 
 @app.route('/map/<string:layer_id>', methods=['POST'])
